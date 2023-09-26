0744cddc079f8ff00f4a975d4e057bab822604e3
handyDominant <acsic666@mail.ru>
2023-09-26 17:21:51 +0300

    First added page

---
+++ Блоковое меню/app.js
@@ -0,0 +1,14 @@
+// 1. Необходимо отобразить кнопку с надписью «Нажми на меня». По клику на нее
+// вывести alert с сообщением
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    alert('Hi')
+});
+
+
+
+
+
